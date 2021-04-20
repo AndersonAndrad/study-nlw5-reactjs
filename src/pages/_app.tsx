18745b24634d70b783a43b8f93e2f185002209f5
AndersonAndrad <anderson_andrade_@outlook.com>
2021-04-20 20:00:40 -0300

    second class
    - create header component
    - create empty player component
    - adding all images
    - adding routes to mock

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+// components
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
+
+// styles
+import '../styles/global.scss'
+import styles from '../styles/app.module.scss';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header/>
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  ) 
+}
+
+export default MyApp;
